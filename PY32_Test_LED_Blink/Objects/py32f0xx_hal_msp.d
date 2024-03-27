db68c8cd1310d423411581fa2944f3192172095f
AllTradesZach <163494283+AllTradesZach@users.noreply.github.com>
2024-03-26 22:31:00 -0600

    Add files via upload

---
+++ PY32_Test_LED_Blink/Objects/py32f0xx_hal_msp.d
@@ -0,0 +1,24 @@
+./objects/py32f0xx_hal_msp.o: Objects\py32f0xx_hal_msp.c Objects\main.h \
+  ..\PY32F0xx_Firmware-master\PY32F0xx_Firmware-master\Drivers\PY32F0xx_HAL_Driver\Inc\py32f0xx_hal.h \
+  Objects\py32f0xx_hal_conf.h \
+  ..\PY32F0xx_Firmware-master\PY32F0xx_Firmware-master\Drivers\PY32F0xx_HAL_Driver\Inc\py32f0xx_hal_rcc.h \
+  ..\PY32F0xx_Firmware-master\PY32F0xx_Firmware-master\Drivers\PY32F0xx_HAL_Driver\Inc\py32f0xx_hal_def.h \
+  C:\Users\zamaz\AppData\Local\Arm\Packs\Puya\PY32F0xx_DFP\1.2.0\Drivers\CMSIS\Device\Puya\PY32F0xx\Include\py32f0xx.h \
+  C:\Users\zamaz\AppData\Local\Arm\Packs\Puya\PY32F0xx_DFP\1.2.0\Drivers\CMSIS\Device\Puya\PY32F0xx\Include\py32f003x6.h \
+  C:\Users\zamaz\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm0plus.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
+  C:\Users\zamaz\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\zamaz\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\zamaz\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Users\zamaz\AppData\Local\Arm\Packs\Puya\PY32F0xx_DFP\1.2.0\Drivers\CMSIS\Device\Puya\PY32F0xx\Include\system_py32f0xx.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdio.h \
+  ..\PY32F0xx_Firmware-master\PY32F0xx_Firmware-master\Drivers\PY32F0xx_HAL_Driver\Inc\py32f0xx_hal_rcc_ex.h \
+  ..\PY32F0xx_Firmware-master\PY32F0xx_Firmware-master\Drivers\PY32F0xx_HAL_Driver\Inc\py32f0xx_hal_gpio.h \
+  ..\PY32F0xx_Firmware-master\PY32F0xx_Firmware-master\Drivers\PY32F0xx_HAL_Driver\Inc\py32f0xx_hal_gpio_ex.h \
+  ..\PY32F0xx_Firmware-master\PY32F0xx_Firmware-master\Drivers\PY32F0xx_HAL_Driver\Inc\py32f0xx_hal_cortex.h \
+  ..\PY32F0xx_Firmware-master\PY32F0xx_Firmware-master\Drivers\PY32F0xx_HAL_Driver\Inc\py32f0xx_hal_dma.h \
+  ..\PY32F0xx_Firmware-master\PY32F0xx_Firmware-master\Drivers\PY32F0xx_HAL_Driver\Inc\py32f0xx_hal_dma_ex.h \
+  ..\PY32F0xx_Firmware-master\PY32F0xx_Firmware-master\Drivers\PY32F0xx_HAL_Driver\Inc\py32f0xx_hal_flash.h \
+  ..\PY32F0xx_Firmware-master\PY32F0xx_Firmware-master\Drivers\PY32F0xx_HAL_Driver\Inc\py32f0xx_hal_pwr.h \
+  ..\PY32F0xx_Firmware-master\PY32F0xx_Firmware-master\Drivers\PY32F0xx_HAL_Driver\Inc\py32f0xx_hal_tim.h \
+  ..\PY32F0xx_Firmware-master\PY32F0xx_Firmware-master\Drivers\PY32F0xx_HAL_Driver\Inc\py32f0xx_hal_tim_ex.h
---
+++ PY32_Test_LED_Blink/Objects/system_py32f0xx.d
@@ -0,0 +1,9 @@
+./objects/system_py32f0xx.o: RTE\Device\PY32F003x6\system_py32f0xx.c \
+  C:\Users\zamaz\AppData\Local\Arm\Packs\Puya\PY32F0xx_DFP\1.2.0\Drivers\CMSIS\Device\Puya\PY32F0xx\Include\py32f0xx.h \
+  C:\Users\zamaz\AppData\Local\Arm\Packs\Puya\PY32F0xx_DFP\1.2.0\Drivers\CMSIS\Device\Puya\PY32F0xx\Include\py32f003x6.h \
+  C:\Users\zamaz\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm0plus.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
+  C:\Users\zamaz\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\zamaz\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\zamaz\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Users\zamaz\AppData\Local\Arm\Packs\Puya\PY32F0xx_DFP\1.2.0\Drivers\CMSIS\Device\Puya\PY32F0xx\Include\system_py32f0xx.h
