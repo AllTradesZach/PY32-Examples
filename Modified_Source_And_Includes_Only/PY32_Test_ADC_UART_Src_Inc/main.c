bea589d4c0ec5b5c7a18d53b6d0bc7df27b3cddf
AllTradesZach <163494283+AllTradesZach@users.noreply.github.com>
2024-03-26 22:39:19 -0600

    Add files via upload

---
+++ Modified_Source_And_Includes_Only/PY32_Test_ADC_UART_Src_Inc/main.c
@@ -0,0 +1,167 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  MCU Application Team
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) Puya Semiconductor Co.
+  * All rights reserved.</center></h2>
+  *
+  * <h2><center>&copy; Copyright (c) 2016 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private define ------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef tim1Handle;
+/* Private user code ---------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+static void APP_SystemClockConfig(void);
+
+/**
+  * @brief  应用程序入口函数.
+  * @retval int
+  */
+int main(void)
+{
+  /* 初始化所有外设，Flash接口，SysTick */
+  HAL_Init();
+  
+  /* 系统时钟配置 */
+  APP_SystemClockConfig(); 
+	
+	gpioInit();
+	timerInit();
+
+  while (1)
+  {
+  }
+}
+
+void gpioInit(void)
+{
+	  GPIO_InitTypeDef GPIO_InitStruct;
+
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  GPIO_InitStruct.Pin = GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+}
+
+void timerInit(void)
+{
+	/* 	Trigger 4x per second, or every 250ms
+			Clock at 24MHz -> 8 million cycles
+			Period to 10,000, prescaler to 800
+			10,000*800=8,000,000 */
+	
+	tim1Handle.Instance = TIM1;																						//Timer 1 advanced timer
+  tim1Handle.Init.Period            = 10000 - 1;												//Timer count = (period+1)*(prescaler+1)
+  tim1Handle.Init.Prescaler         = 800 - 1;
+  tim1Handle.Init.ClockDivision     = TIM_CLOCKDIVISION_DIV1;						//Use full clock rate
+  tim1Handle.Init.CounterMode       = TIM_COUNTERMODE_UP;
+  tim1Handle.Init.RepetitionCounter = 1 - 1;
+  tim1Handle.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+	
+  if (HAL_TIM_Base_Init(&tim1Handle) != HAL_OK)
+  {
+    APP_ErrorHandler();
+  }
+
+  if (HAL_TIM_Base_Start_IT(&tim1Handle) != HAL_OK)
+  {
+    APP_ErrorHandler();
+  }
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_5);
+}
+
+/**
+  * @brief  系统时钟配置函数
+  * @param  无
+  * @retval 无
+  */
+static void APP_SystemClockConfig(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /* 振荡器配置 */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE | RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_LSI; /* 选择振荡器HSE,HSI,LSI */
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;                          /* 开启HSI */
+  RCC_OscInitStruct.HSIDiv = RCC_HSI_DIV1;                          /* HSI 1分频 */
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_24MHz;  /* 配置HSI时钟8MHz */
+  RCC_OscInitStruct.HSEState = RCC_HSE_OFF;                         /* 关闭HSE */
+  /*RCC_OscInitStruct.HSEFreq = RCC_HSE_16_32MHz;*/
+  RCC_OscInitStruct.LSIState = RCC_LSI_OFF;                         /* 关闭LSI */
+
+  /* 配置振荡器 */
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    APP_ErrorHandler();
+  }
+
+  /* 时钟源配置 */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1; /* 选择配置时钟 HCLK,SYSCLK,PCLK1 */
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI; /* 选择HSI作为系统时钟 */
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;     /* AHB时钟 1分频 */
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;      /* APB时钟 1分频 */
+  /* 配置时钟源 */
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    APP_ErrorHandler();
+  }
+}
+
+/**
+  * @brief  错误执行函数
+  * @param  无
+  * @retval 无
+  */
+void APP_ErrorHandler(void)
+{
+  /* 无限循环 */
+  while (1)
+  {
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  输出产生断言错误的源文件名及行号
+  * @param  file：源文件名指针
+  * @param  line：发生断言错误的行号
+  * @retval 无
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* 用户可以根据需要添加自己的打印信息,
+     例如: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* 无限循环 */
+  while (1)
+  {
+  }
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT Puya *****END OF FILE******************/
